5e6da537908a513935a004d23f710f8d35c23b70
ElizaKhanova <hanova_eliza19981007@mail.ru>
2023-02-19 23:15:22 +0200

    add changes

--- index.js
+++ index.js
@@ -1,52 +1,90 @@
-
 // Создаем переменную для массива из объектов
-// let directors = [
-// {
-//     name: 'Стивен Спилберг',
-//     career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
-//     films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
-//     top_rated_film: 'Список Шиндлера'
-// },
-// { 
-//     name: 'Кристофер Нолан',
-//     career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
-//     films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
-//     top_rated_film: 'Начало'
-// },
-// {
-//     name: 'Мартин МакДона',
-//     career: 'Сценарист, Режиссёр, Продюсер',
-//     films: 'https://www.film.ru/person/martin-makdonah',
-//     top_rated_film: 'Три билборда на границе Эббинга, Миссури'
-// },
-// {
-//     name: 'Алексей Балабанов',
-//     career: 'Режиссёр, Сценарист, Актёр, Продюсер',
-//     films: 'https://www.film.ru/person/aleksey-balabanov',
-//     top_rated_film: 'Брат'
-// },
-// {
-//     name: 'Питер Фаррелли',
-//     career: 'Продюсер, Режиссёр, Сценарист, Актёр',
-//     films: 'https://www.film.ru/person/piter-farrelli',
-//     top_rated_film: 'Зелёная книга'
-// },
-// {
-//     name: 'Юрий Быков',
-//     career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
-//     films: 'https://www.film.ru/person/yuriy-bykov',
-//     top_rated_film: 'Дурак'
-// },
-// {
-//     name: 'Жан-Марк Валле',
-//     career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
-//     films: 'https://www.film.ru/person/zhan-mark-valle',
-//     top_rated_film: 'Далласский клуб покупателей'
-// },
-// ];
-let myCollection = document.getElementsByClassName("name");
-let i;
-let names = ["masha", "sasha"]
-for (i = 0; i < myCollection.length; i++) {
-myCollection[i].innerHTML = names[1];
+let directors = [
+{
+    name: 'Стивен Спилберг',
+    career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+    films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+    top_rated_film: 'Список Шиндлера'
+},
+{ 
+    name: 'Кристофер Нолан',
+    career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+    films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+    top_rated_film: 'Начало'
+},
+{
+    name: 'Мартин МакДона',
+    career: 'Сценарист, Режиссёр, Продюсер',
+    films: 'https://www.film.ru/person/martin-makdonah',
+    top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+},
+{
+    name: 'Алексей Балабанов',
+    career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+    films: 'https://www.film.ru/person/aleksey-balabanov',
+    top_rated_film: 'Брат'
+},
+{
+    name: 'Питер Фаррелли',
+    career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+    films: 'https://www.film.ru/person/piter-farrelli',
+    top_rated_film: 'Зелёная книга'
+},
+{
+    name: 'Юрий Быков',
+    career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+    films: 'https://www.film.ru/person/yuriy-bykov',
+    top_rated_film: 'Дурак'
+},
+{
+    name: 'Жан-Марк Валле',
+    career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+    films: 'https://www.film.ru/person/zhan-mark-valle',
+    top_rated_film: 'Далласский клуб покупателей'
+},
+];
+
+//Создаем карточку каждого режиссера. 
+//Сначала общий контейнер для элементов, затем закладываем в эти контейеры поочередно имя, карьеру, фильмографию
+
+function createCard (){
+    const directorList = document.querySelector ('.directors-list');
+    console.log (directorList);
+    directors.forEach((item)=> {
+        const card = document.createElement ('div');
+        card.className = 'director-card';
+        directorList.prepend(card);
+
+        const directorName = document.createElement ('div');
+        directorName.className = 'director-card__name';
+        card.append (directorName);
+        directorName.innerHTML = item.name;
+
+        const directorJob = document.createElement ('div');
+        directorJob.className = 'director-card__job';
+        card.append (directorJob);
+        directorJob.innerHTML = item.career;
+
+        const directorFilmography = document.createElement ('a');
+        directorFilmography.className = 'director-card__films';
+        directorFilmography.href = item.films;
+        card.append (directorFilmography);
+        directorFilmography.innerHTML = 'Фильмография';
+
+    })
 }
+
+createCard ()
+
+function createTopFilmsfilmsCollection() {
+    let topFilms = directors.map(el => el.top_rated_film);
+    console.log (topFilms)
+
+    let filmsWrap = document.querySelector('.top-films');
+    filmsCollection = document.createElement('p');
+    filmsCollection.className = 'top-films__collection';
+    filmsCollection.innerHTML = topFilms;
+    filmsWrap.append(filmsCollection);
+}
+createTopFilmsfilmsCollection()
+
